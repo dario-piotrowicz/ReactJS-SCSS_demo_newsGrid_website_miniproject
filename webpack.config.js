c7a6357ae209aa6a688af56ad9ec2377989ced8f
Dario Piotrowicz <dario.piotrowicz@gmail.com>
2020-06-20 00:47:43 +0200

    typescript setup

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,33 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/index.jsx', //or ./src/index.js
+  entry: './src/index.tsx', //or ./src/index.ts
   output: {
     path: path.join(__dirname, 'public'),
-    filename: 'bundle.js'
+    filename: 'bundle.js',
   },
   devServer: {
     contentBase: './public',
   },
   module: {
     rules: [
-    {
-      test: /\.(js|jsx)$/,
-      exclude: /node_modules/,
-      use: ['babel-loader', 'eslint-loader']
-    },
-    {
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader', 'eslint-loader'],
+      },
+      {
+        test: /\.(ts|tsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader', 'ts-loader', 'eslint-loader'],
+      },
+      {
         test: /\.(scss)$/,
         use: ['style-loader', 'css-loader', 'sass-loader'],
       },
-    ]
+    ],
   },
   resolve: {
-    extensions: ['.js', '.jsx'],
+    extensions: ['.js', '.jsx', '.ts', '.tsx'],
   },
 };
