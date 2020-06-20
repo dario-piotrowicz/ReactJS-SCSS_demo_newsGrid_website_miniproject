63dc04471a2e71ff19e0407ee90c6aa2bf2e8acc
Dario Piotrowicz <dario.piotrowicz@gmail.com>
2020-06-20 15:16:17 +0200

    implemented header

--- src/components/header/header.component.tsx
+++ src/components/header/header.component.tsx
@@ -1,8 +1,42 @@
 import React, { FunctionComponent } from 'react';
 import './header.styles.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
+
+const githubBaseUrl = 'https://github.com/dario-piotrowicz/';
+const githubRepoName = 'ReactJS-SCSS_demo_newsGrid_website_miniproject';
+const githubRepoUrl = `${githubBaseUrl}${githubRepoName}`;
 
 const Header: FunctionComponent = () => {
-  return <h1>Hello World! from header</h1>;
+  return (
+    <header id="main-header">
+      <div className="centering-container">
+        <div className="logo">
+          <img src="./images/logo.png" />
+          <h1>
+            News<span>Grid</span>
+          </h1>
+        </div>
+        <div className="social">
+          <a href={githubRepoUrl} target="_blank">
+            <FontAwesomeIcon icon={faGithub} size="2x" />
+          </a>
+        </div>
+        <nav>
+          <ul>
+            <li>
+              <a className="current" href="#">
+                Home
+              </a>
+            </li>
+            <li>
+              <a href="#">About</a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
