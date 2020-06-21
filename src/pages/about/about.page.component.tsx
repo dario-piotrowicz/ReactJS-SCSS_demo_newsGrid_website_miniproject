8e301daadb744de984af7c6c5fc8367f6d2a09c0
Dario Piotrowicz <dario.piotrowicz@gmail.com>
2020-06-21 16:24:58 +0200

    implemented about page

--- src/pages/about/about.page.component.tsx
+++ src/pages/about/about.page.component.tsx
@@ -3,8 +3,45 @@ import './about.styles.scss';
 
 const About: FunctionComponent = () => {
   return (
-    <div id="about-page">
-      <h1>Empty About Page</h1>
+    <div id="about-page" className="centering-container">
+      <article>
+        <h1>About Our Organization</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate et
+          rem excepturi rerum soluta eius consectetur cupiditate nostrum eum,
+          enim itaque, minima nisi aliquam quidem perspiciatis. Recusandae
+          consequatur vero ratione quas molestiae, saepe, aliquam, incidunt
+          laudantium dignissimos quis delectus similique fugit repellat iusto
+          facilis eaque a repellendus earum. Sunt, deleniti.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto,
+          voluptates obcaecati. Molestiae pariatur enim veritatis eligendi
+          ratione dolorem quasi. Optio nisi officiis perferendis accusamus
+          repellat quibusdam animi enim et vero deleniti ipsam molestias porro
+          illo modi rem odio fugit excepturi, laudantium aperiam minima facere.
+          Eveniet, placeat ratione molestias, vitae blanditiis sapiente optio
+          pariatur magnam libero cumque porro. Quisquam culpa recusandae
+          temporibus in, obcaecati corporis maxime incidunt blanditiis nemo
+          quaerat consequuntur eligendi quis, iusto ipsam! Quasi, magni
+          aspernatur? Necessitatibus molestias in illo quo blanditiis cumque?
+          Mollitia dolorem tenetur optio inventore rerum blanditiis
+          necessitatibus ipsa, reprehenderit aut exercitationem repellat non
+          aliquam a, veniam nihil sapiente officia itaque! Consectetur assumenda
+          vero voluptatibus magnam, ipsa tempora quisquam aliquam a laboriosam
+          culpa officiis voluptates unde nisi blanditiis quo deserunt esse
+          dolorem earum at beatae quaerat quasi nemo quam veritatis. Neque,
+          inventore odio rem modi quas ipsam labore incidunt natus sed eius
+          aperiam reiciendis recusandae harum?
+        </p>
+      </article>
+      <aside>
+        <h2>Join Our Club</h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum, odio.
+        </p>
+        <button className="btn">Join Now</button>
+      </aside>
     </div>
   );
 };
