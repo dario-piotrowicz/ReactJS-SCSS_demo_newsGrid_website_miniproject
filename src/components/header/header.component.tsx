5e366e3f6e8f32b616335b55f649848b06ce2423
Dario Piotrowicz <dario.piotrowicz@gmail.com>
2020-06-22 13:52:35 +0200

    made header logo a link to home

--- src/components/header/header.component.tsx
+++ src/components/header/header.component.tsx
@@ -2,7 +2,7 @@ import React, { FunctionComponent } from 'react';
 import './header.styles.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGithub } from '@fortawesome/free-brands-svg-icons';
-import { NavLink } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 
 const githubBaseUrl = 'https://github.com/dario-piotrowicz/';
 const githubRepoName = 'ReactJS-SCSS_demo_newsGrid_website_miniproject';
@@ -12,12 +12,12 @@ const Header: FunctionComponent = () => {
   return (
     <header id="main-header">
       <div className="centering-container">
-        <div className="logo">
+        <Link to="/" className="logo">
           <img src="./images/logo.png" />
           <h1>
             News<span>Grid</span>
           </h1>
-        </div>
+        </Link>
         <div className="social">
           <a href={githubRepoUrl} target="_blank">
             <FontAwesomeIcon icon={faGithub} size="2x" />
