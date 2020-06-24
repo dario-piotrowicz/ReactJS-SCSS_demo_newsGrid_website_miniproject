a301370d691d88c4e4c875f8f3ea3991e74d1344
Dario Piotrowicz <dario.piotrowicz@gmail.com>
2020-06-24 22:47:40 +0200

    testing if browserRouter works correctly on netlify

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React, { FunctionComponent } from 'react';
 import ReactDOM from 'react-dom';
 import App from './app';
-import { HashRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 const NewsGrid: FunctionComponent = () => {
   return (
