b1f564e974bb80661d3861028aa96b953d897bfe
Dario Piotrowicz <dario.piotrowicz@gmail.com>
2020-06-27 11:59:29 +0200

    using HashRouter in order to avoid redirection issues (+ bundle update for deployment)

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React, { FunctionComponent } from 'react';
 import ReactDOM from 'react-dom';
 import App from './app';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 
 const NewsGrid: FunctionComponent = () => {
   return (
